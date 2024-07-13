f3a39a4891a299791bef4bd55f903b8880be1a0f
Zakir <Zakoota11@hotmail.com>
2024-07-13 08:31:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,16 @@
 # Update package list
 sudo apt-get update
 
-# Install dependencies for libopenblas-base
-sudo apt-get install -y libc6 libgfortran5
+# Install dependencies with the correct architecture
+sudo apt-get install -y libc6:armhf libgfortran5:armhf
+
+# Fix any broken dependencies
+sudo apt-get install -f -y
 
 # Install libopenblas-base package
 sudo dpkg -i libopenblas-base_0.3.5+ds-3+rpi1_armhf.deb
 
-# Fix any broken dependencies
+# Fix any broken dependencies again
 sudo apt-get install -f -y
 
 # Install libopenblas-dev package
