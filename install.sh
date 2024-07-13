669b7587ad14b9858fc541757e7f721a9cf94b19
Zakir <Zakoota11@hotmail.com>
2024-07-13 08:28:47 +0800

    Update install.sh
    change install.sh for installing dependencies for proper installation

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# Update package list
+sudo apt-get update
+
+# Install dependencies for libopenblas-base
+sudo apt-get install -y libc6 libgfortran5
+
+# Install libopenblas-base package
 sudo dpkg -i libopenblas-base_0.3.5+ds-3+rpi1_armhf.deb
+
+# Fix any broken dependencies
+sudo apt-get install -f -y
+
+# Install libopenblas-dev package
 sudo dpkg -i libopenblas-dev_0.3.5+ds-3+rpi1_armhf.deb
+
+# Fix any broken dependencies again
+sudo apt-get install -f -y
